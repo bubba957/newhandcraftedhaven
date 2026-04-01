58644c37793e51675af39d7b18c37c4ffc49f11f
ammonmerrell <ammonmerrell@outlook.com>
2026-04-01 10:32:53 -0600

    moved code into file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,6 +53,54 @@ export default function Home() {
   );
 }
 
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Quintessential, Oswald, Fascinate } from "next/font/google";
+
+const quintessential = Quintessential({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const oswald = Oswald({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const fascinate = Fascinate({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={styles.quintessential}>Handcrafted Haven</h1>
+      <Image className={styles.picture}
+        src="/single-handmade-pot.avif"
+        alt="Next.js logo"
+        width={250}
+        height={250}
+        priority
+      />
+      <p>Come one come all to our fantabulous site where you can
+        not only look at but also buy the wonderfully handcrafted 
+        item of your choice. That's right. And if you have any old
+        junk that is just cluttering up the insides of your place
+        then one day YOU may see those items here on this magnificent
+        website where they'll be sold according to the price YOU want.</p>
+
+        <p className={styles.underlying}>(These items can only be sold after they have 
+        been created. Also a portion of the profits will be going to us
+        after everythings said and done.)</p>
+    </main>
+  );
+}
+
+
 // export default function Home() {
 //   return (
 //     <div className={styles.page}>
@@ -116,6 +164,7 @@ export default function Home() {
 //     </div>
 //   );
 // }
+
 
 // export default function Home() {
 //   return (<div className={styles.page}><h1> Handcrafted Haven</h1></div>
