a81413577e3fdda8d04279bec04434018612c48a
ammonmerrell <ammonmerrell@outlook.com>
2026-04-01 10:26:24 -0600

    added in correct code

--- src/app/ui/revinue-chart.tsx
+++ src/app/ui/revinue-chart.tsx
@@ -1,8 +1,8 @@
 import { generateYAxis } from '@/app/lib/utils';
 // import { CalendarIcon } from '@heroicons/react/24/outline';
 // import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
-import { fetchRevenue } from '@/app/lib/data';
+import { product } from '@/app/lib/definitions';
+import { fetchProduct } from '@/app/lib/data';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -11,15 +11,15 @@ import { fetchRevenue } from '@/app/lib/data';
 // https://airbnb.io/visx/
 
 
-export default async function RevenueChart() { 
-  const revenue = await fetchRevenue(); 
+export default async function ProductChart() { 
+  const product = await fetchProduct(); 
 
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
-  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+  const { yAxisLabels, topLabel } = generateYAxis(product);
 
-  if (!revenue || revenue.length === 0) {
+  if (!product || product.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
 
@@ -41,13 +41,11 @@ export default async function RevenueChart() {
             ))}
           </div>
 
-          {revenue.map((month) => (
+          {product.map((month) => (
             <div key={month.name} className="flex flex-col items-center gap-2">
               <div
                 className="w-full rounded-md bg-blue-300"
-                style={{
-                  height: `${(chartHeight / topLabel) * month.name}px`,
-                }}
+                
               ></div>
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
                {month.name} {month.description}
