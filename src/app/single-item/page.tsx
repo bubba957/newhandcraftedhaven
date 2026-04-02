28bd78bdd5abde9db843d093038c5e8c7ead8693
Cody Smith <scody49@gmail.com>
2026-04-02 10:11:26 -0700

    change comment

--- src/app/single-item/page.tsx
+++ src/app/single-item/page.tsx
@@ -45,11 +45,11 @@
 //         />
 //         <div>
 //           <p><span className={styles.title}>Handmade Studio Pottery Mug Drip Glaze Earth Tone Artisan Cup</span>
-//           <br></br><br></br><br></br>
+//           {/* <br></br><br></br><br></br> */}
 //           <span className={styles.artist}>Scout and Shore</span>
-//           <br></br><br></br><br></br>
+//           {/* <br></br><br></br><br></br> */}
 //           <span className={styles.price}>Price: $32.00</span></p>
-//           <br></br><br></br>
+//           {/* <br></br><br></br> */}
 //           <button className={styles.btn}>Add To Cart</button>
 //         </div>
 //       </div>
@@ -72,19 +72,9 @@ export default async function Page() {
     //   const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
-      <h1 className={`${quintessential} mb-4 text-xl md:text-2xl`}>
+      {/* <h1 className={`${quintessential} mb-4 text-xl md:text-2xl`}>
         Dashboard
-      </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
-      </div>
+      </h1> */}
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <ProductChart />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
